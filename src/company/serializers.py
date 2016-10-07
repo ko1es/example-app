4a27dba5250773f813e9a4d5c5fd6bb4baacf3c1
Nikita Kolesnikov <koles.web@gmail.com>
2016-10-07 19:14:07 +0700

    flake8 comments for serializers

--- src/company/serializers.py
+++ src/company/serializers.py
@@ -1,42 +1,61 @@
 # -*- coding: utf-8 -*-
+"""Company app serializers."""
 from rest_framework import serializers
 
 from company import models
 
 
 class PhoneSerializer(serializers.ModelSerializer):
+    """Phone views serializer."""
+
     class Meta:
+        """Meta class."""
+
         model = models.CompanyPhone
         fields = ('phone', )
 
 
 class ParentRubricSerializer(serializers.ModelSerializer):
+    """ParentRubric serializer."""
+
     class Meta:
+        """Meta class."""
+
         model = models.Rubric
         fields = ('id', 'name')
 
 
 class RubricSerializer(serializers.ModelSerializer):
+    """Rubric serializer."""
+
     parent = ParentRubricSerializer()
 
     class Meta:
+        """Meta class."""
+
         model = models.Rubric
         fields = ('id', 'name', 'parent')
 
 
 class BuildingSerializer(serializers.ModelSerializer):
+    """Building serializer."""
+
     class Meta:
+        """Meta class."""
+
         model = models.Building
         fields = ('address', 'lat', 'lon')
 
 
 class CompanySerializer(serializers.ModelSerializer):
+    """Company serializer."""
+
     phones = serializers.StringRelatedField(many=True)
     rubrics = serializers.StringRelatedField(many=True)
     building = BuildingSerializer()
 
     class Meta:
+        """Meta class."""
+
         model = models.Company
         fields = ('id', 'name', 'phones', 'building', 'rubrics')
-
-    # todo: building -> building.address
